0be24ba03f5c5d0d9539b12ce523cd7193a7cc67
SouthNova <jsn950509@gmail.com>
2025-05-08 02:25:24 +0900

    refactor: 구글 드라이브 인증 메서드 init으로 이동

--- main.py
+++ main.py
@@ -14,26 +14,16 @@ log = get_logger()
 
 class Main:
     def __init__(self):
+        self._try_google_drive_auth()
+
         self.location = self._input_location()
         self.keywords = ["강아지 유치원", "반려견 유치원", "강아지 호텔", "반려견 호텔", "애견 유치원", "애견 호텔"]
 
         self.max_retries = 3    # 크롤링 실패 시, 재시도 횟수
         self.retry_delay = 1    # 재시도 전, 딜레이 (초 단위)
 
-    def _input_location(self):
-        while True:
-            location = input("검색할 지역을 입력하세요 (예: 서초구, 강남구 등): ").strip()
-            if location:
-                break
-            print("지역을 입력해주세요. 빈 값은 허용되지 않습니다.")
-        return location
-
     async def run(self):
-        log.info("크롤링 작업 시작")
         start_time = time.time()
-
-        upload_google_drive = UploadGoogleDrive()
-        upload_google_drive.google_drive_api_auth()
         
         # 네이버 지도 검색 결과 가져오기
         try:
@@ -57,7 +47,7 @@ class Main:
             results = merge_dict_lists_by_key('id', places_data, crawl_result)
 
             # 결과를 엑셀로 저장
-            dict_list_to_excel(results, self.location, output_dir='/app/output')
+            dict_list_to_excel(results, self.location, output_dir='./output')
             
             # 실행 시간 로깅
             elapsed_time = time.time() - start_time
@@ -66,10 +56,21 @@ class Main:
         except Exception as e:
             log.error(f"크롤링 중 오류 발생: {e}")
 
-
     async def _fetch_place_data(self) -> List[Dict[str, Any]]:
         """장소 데이터를 가져오는 함수"""
         return fetch_naver_place_list(self.location, self.keywords)
+
+    def _try_google_drive_auth(self):
+        upload_google_drive = UploadGoogleDrive()
+        upload_google_drive.google_drive_api_auth()
+
+    def _input_location(self):
+        while True:
+            location = input("검색할 지역을 입력하세요 (예: 서초구, 강남구 등): ").strip()
+            if location:
+                break
+            print("지역을 입력해주세요. 빈 값은 허용되지 않습니다.")
+        return location
     
 
 if __name__ == "__main__":
